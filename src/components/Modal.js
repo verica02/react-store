6f96b2069bf4a7f645fd2d47e4cc3c40528f4c24
verica02 <77109037+verica02@users.noreply.github.com>
2021-03-20 16:41:29 +0100

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -24,7 +24,7 @@ export default class Modal extends Component {
                             <h5>Додадено во кошничка</h5>
                             <img src={img} className="img-fluid" alt="product" />
                             <h5>{title}</h5>
-                            <h5 className="text-muted">price : {price} ден.</h5>
+                            <h5 className="text-muted">цена : {price} ден.</h5>
                             <Link to="/">
                                 <ButtonContainer onClick={()=>closeModal()}>
                                     назад
